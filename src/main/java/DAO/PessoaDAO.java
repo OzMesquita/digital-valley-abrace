dfe1b54f0332c8e6e2ed26bfc2d6319501888ea7
Alexandre <franciscoalexandre.m.c@gmail.com>
2017-10-11 16:25:03 -0300

    Atualizações nos métodos cadastrarPessoa, listarPessoas, editarPessoa e excluirPessoa da classe PessoaDAO.

--- src/main/java/DAO/PessoaDAO.java
+++ src/main/java/DAO/PessoaDAO.java
@@ -6,77 +6,105 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 import exceptions.PessoaInvalidaException;
+import model.Assistido;
 import model.Pessoa;
 
-public class PessoaDAO extends ExecutaSQL {
-
+public class PessoaDAO extends ExecutaSQL{
+	
 	public PessoaDAO(Connection connection) {
 		super(connection);
 	}
-
-	// Incompleto
-	@SuppressWarnings("deprecation")
-
-	public int cadastrarPessoa(Pessoa pessoa) throws SQLException, PessoaInvalidaException {
+	
+	public int cadastrarPessoa(Pessoa pessoa) throws SQLException {
 		PreparedStatement stmt = null;
-		String sql = "insert into ABRACE.PESSOA" + "(ativo, datacadastro, email, telefone2, telefone1, endereco, nome)"
-				+ "values (?, ?, ?, ?, ?, ?, ?)";
-		try {
-			// prepared statement para inserção
-			getConexao().setAutoCommit(false);
-			stmt = getConexao().prepareStatement(sql);
-			// seta os valores
-			stmt.setBoolean(1, pessoa.isAtivo());
-			pessoa.setDataCadastro(LocalDate.now());
-			stmt.setDate(2, new Date(pessoa.getDataCadastro().getYear(), pessoa.getDataCadastro().getMonthValue() + 1,
-					pessoa.getDataCadastro().getDayOfMonth()));
-			stmt.setString(3, pessoa.getEmail());
-			stmt.setString(4, pessoa.getTelefone2());
-			stmt.setString(5, pessoa.getTelefone());
-			stmt.setString(6, pessoa.getEndereco());
-			stmt.setString(7, pessoa.getNome());
-			// executa
-			stmt.execute();
-			getConexao().commit();
-		} catch (SQLException e) {
-			if (getConexao() != null) {
-				System.out.println("Transação está retornando ao estado anterior");
+        String sql = "insert into ABRACE.PESSOA (ativo, datacadastro, email, telefone2, telefone1, endereco, nome) values (?, ?, ?, ?, ?, ?, ?)";
+        int id = 0;
+        try {
+            // prepared statement for insertion
+            getConexao().setAutoCommit(false);
+            stmt = getConexao().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            // set values
+            stmt.setBoolean(1, pessoa.isAtivo());
+            stmt.setDate(2, Date.valueOf(pessoa.getDataCadastro()));
+            stmt.setString(3, pessoa.getEmail());
+            stmt.setString(4, pessoa.getTelefone2());
+            stmt.setString(5, pessoa.getTelefone());
+            stmt.setString(6, pessoa.getEndereco());
+            stmt.setString(7, pessoa.getNome());
+            // execute
+            stmt.execute();
+            getConexao().commit();
+            // get DB id
+	        ResultSet rs = stmt.getGeneratedKeys();
+	        if(rs.next()){
+	            id = rs.getInt(1);
+	        }
+        } catch (SQLException e) {
+            if (getConexao() != null) {
+                System.out.println(e.getMessage() + " Transação está retornando ao estado anterior");
 				getConexao().rollback();
-			}
-			throw new RuntimeException(e);
-		} finally {
-			if (stmt != null) {
-				stmt.close();
-			}
-			getConexao().setAutoCommit(true);
-		}
-		String sql2 = "select idpessoa from pessoa where nome=" + pessoa.getNome();
-		int idpessoa;
+            }
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+            getConexao().setAutoCommit(true);
+        }
+        return id;
+			
+	}
+	
+	public ArrayList<Pessoa> listarPessoas(Boolean situacao) throws PessoaInvalidaException{
+		ArrayList<Pessoa> listaPessoas = new ArrayList<Pessoa>();
+		String sql = "SELECT * FROM ABRACE.Pessoa where ativo = "+situacao;
 		try {
-			// prepared statement para inserção
-			getConexao().setAutoCommit(false);
-			stmt = getConexao().prepareStatement(sql2);
-			// seta os valores
-			idpessoa = stmt.executeQuery().getInt(0);
-			// executa
-			getConexao().commit();
-		} catch (SQLException e) {
-			if (getConexao() != null) {
-				System.out.println("Transação está retornando ao estado anterior");
-				getConexao().rollback();
+			PreparedStatement stmt = getConexao().prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				int id = rs.getInt(1);
+				String nome = rs.getString(2);
+				String endereco = rs.getString(3);
+				String telefone1 = rs.getString(4);
+				String telefone2 = rs.getString(5);
+				String email = rs.getString(6);
+				LocalDate dataCadastro = rs.getDate(7).toLocalDate();
+				listaPessoas.add(new Pessoa(id, nome, endereco,  dataCadastro, telefone1, telefone2, email, situacao));
 			}
-			throw new RuntimeException(e);
-		} finally {
-			if (stmt != null) {
-				stmt.close();
-			}
-			getConexao().setAutoCommit(true);
+			stmt.close();
+		} catch (SQLException ex) {
+			throw new RuntimeException(ex.getMessage());
 		}
-		return idpessoa;
+		return listaPessoas;
 	}
 
+	public void editarPessoa(Pessoa pessoa) throws SQLException {
+		String sql= "UPDATE ABRACE.PESSOA SET nome=?, endereco=?, telefone1=?, telefone2=?, email=?, dataCadastro=?, ativo=? WHERE idPessoa="+pessoa.getId();
+		PreparedStatement stmt = getConexao().prepareStatement(sql);
+		   
+		stmt.setString (1, pessoa.getNome());
+		stmt.setString (2, pessoa.getEndereco());
+		stmt.setString (3, pessoa.getTelefone());
+		stmt.setString (4, pessoa.getTelefone2());
+		stmt.setString (5, pessoa.getEmail());
+		stmt.setDate   (6, Date  .valueOf(pessoa.getDataCadastro()));
+		stmt.setBoolean(7, pessoa.isAtivo());
+		    	
+		stmt.execute();
+		stmt.close();
+	}
+	public void excluirPessoa(Pessoa pessoa) throws SQLException{
+		String sql= "UPDATE ABRACE.PESSOA SET ativo=false WHERE idPessoa="+pessoa.getId();
+	    PreparedStatement stmt = getConexao().prepareStatement(sql);
+   
+	    stmt.setBoolean(1, pessoa.isAtivo());
+	    	
+	    stmt.execute();
+	    stmt.close();
+	}
+	   
 }
