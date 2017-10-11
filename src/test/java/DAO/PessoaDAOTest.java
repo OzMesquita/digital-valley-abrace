2a2833b38186fad9be6aa36f23ed9261bc483ab9
Tagila <tagila.lima96@gmail.com>
2017-10-11 14:44:54 -0300

    Inclusão de classes de teste PessoaDAOTest, PessoaFisicaDAOTest e PessoaJuridicaDAOTest na SprintAtual.

---
+++ src/test/java/DAO/PessoaDAOTest.java
@@ -0,0 +1,26 @@
+package DAO;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import exceptions.PessoaInvalidaException;
+import exceptions.PessoaJuridicaInvalidaException;
+import model.Pessoa;
+
+public class PessoaDAOTest {
+
+	public static void main(String[] args) throws PessoaInvalidaException, PessoaJuridicaInvalidaException, SQLException {
+		Pessoa p = new Pessoa();
+		
+		p.setNome("Pessoa Test");
+		p.setEndereco("Rua de teste");
+		p.setTelefone("78965412");
+		p.setTelefone2("78945612");
+		
+		p.setDataCadastro(LocalDate.now());
+		p.setEmail("mm@gmail.com");
+		p.setAtivo(true);
+		
+		new PessoaDAO(new ConnectionFactory().getConnection()).cadastrarPessoa(p);
+	}
+}
