87c9c0984c92777d1eabef8c6d8565443ac772a5
Isabelly Lima <isabelly.lima132@gmail.com>
2017-10-23 15:02:19 -0300

    Atualização do tamanha do cnpj no script de criação do banco de dados.

--- src/main/java/DAO/Instalacao.java
+++ src/main/java/DAO/Instalacao.java
@@ -38,7 +38,7 @@ public class Instalacao {
 				   //-----------------------------------//
 				   "CREATE TABLE ABRACE.Pessoa_Juridica (" + 
 				   " idPessoa INT NOT NULL," + 
-				   " cnpj VARCHAR(16) NOT NULL," +
+				   " cnpj VARCHAR(32) NOT NULL," +
 				   " razaoSocial VARCHAR(128)," +
 				   " fantasia VARCHAR(128)," + 
 				   " PRIMARY KEY (idPessoa)," + 
