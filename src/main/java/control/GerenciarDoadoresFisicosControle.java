d025885a45df4d218c2513bccb297ed8c1c6214d
Jhean <jheanmarllos1@gmail.com>
2017-10-27 10:04:35 -0300

    Criando telas de listagem de doadores e linkando com Fachada e controle.

---
+++ src/main/java/control/GerenciarDoadoresFisicosControle.java
@@ -0,0 +1,122 @@
+package control;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.TableItem;
+
+import facade.AssistidoFachada;
+import facade.DoadorFachada;
+import model.Assistido;
+import model.PessoaFisica;
+import view.EditarAssistidoView;
+import view.GerenciarAssistidosView;
+import view.GerenciarDoadoresFisicosView;
+
+public class GerenciarDoadoresFisicosControle {
+	private GerenciarDoadoresFisicosView view; 
+	private DoadorFachada fachada;
+	private ArrayList<PessoaFisica> listaTodosDoadoresFisicos;
+	private ArrayList<PessoaFisica> listaExibidaNaTabela;
+	
+	public GerenciarDoadoresFisicosView getView() {
+		return view;
+	}
+
+
+	public void setView(GerenciarDoadoresFisicosView view) {
+		this.view = view;
+	}
+
+
+	public DoadorFachada getFachada() {
+		return fachada;
+	}
+
+
+	public void setFachada(DoadorFachada fachada) {
+		this.fachada = fachada;
+	}
+	
+	public GerenciarDoadoresFisicosControle (GerenciarDoadoresFisicosView view) {
+		setView(view);
+		setFachada(new DoadorFachada());
+		
+	}
+	
+	public GerenciarDoadoresFisicosControle () {
+	}
+
+	public void excluirLinhasDaTabela() {
+		view.getTable().removeAll();
+	}
+	
+	public ArrayList<PessoaFisica> obterTodosDoadoresFisicos() {
+		excluirLinhasDaTabela();
+		listaTodosDoadoresFisicos = fachada.listarPessoaFisica();
+		return listaTodosDoadoresFisicos;
+	}
+	
+	public void preencherTabelaDoadoresFisicos(ArrayList<PessoaFisica> doadores) {
+		excluirLinhasDaTabela();
+		DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		listaExibidaNaTabela = doadores;
+		for(int i = 0; i < doadores.size(); i++) {
+			TableItem item = new TableItem(view.getTable(), SWT.NONE);
+			item.setText(0, Integer.toString(doadores.get(i).getId()));
+			item.setText(1, doadores.get(i).getNome());
+			item.setText(2, doadores.get(i).getDataNasc().format(formatador));
+			item.setText(3, doadores.get(i).getCpf());
+		}	
+	}
+	
+	public boolean confirmacao() {
+        MessageBox messageBox = new MessageBox(view.getShlGerenciarDoadoresFisicos(),SWT.ICON_WARNING | SWT.CANCEL | SWT.OK);
+        
+        messageBox.setText("Aviso!");
+        messageBox.setMessage("Você deseja realmente deletar o assistido do sistema?");
+        int buttonID = messageBox.open();
+        switch(buttonID) {
+          case SWT.CANCEL:
+        	  return false;
+          case SWT.OK:
+            return true;
+          default:
+        	  return false;
+        }
+        
+      }
+	
+	public ArrayList<PessoaFisica> pesquisarAssistidos(String nomePesquisa) {
+		excluirLinhasDaTabela();
+		ArrayList<PessoaFisica> listaPesquisaDoadoresFisicos = new ArrayList<PessoaFisica>();
+		
+		for(int i = 0; i < listaTodosDoadoresFisicos.size(); i++) {
+			if(listaTodosDoadoresFisicos.get(i).getNome().toLowerCase().contains(nomePesquisa.toLowerCase())) {
+				listaPesquisaDoadoresFisicos.add(listaTodosDoadoresFisicos.get(i));
+			}
+		}
+		return listaPesquisaDoadoresFisicos;
+	}
+	
+	public void getEvent(SelectionEvent event) {
+		if (event.getSource().toString().equals("Button {Pesquisar}")) {
+			preencherTabelaDoadoresFisicos(pesquisarAssistidos(view.getTfPesquisa().getText()));
+		}
+		if (event.getSource().toString().equals("Button {Editar Assistido}")) {
+			PessoaFisica a = fachada.obterDoadorFisico(listaExibidaNaTabela.get(view.getTable().getSelectionIndex()).getId());
+			this.view.getShlGerenciarDoadoresFisicos().dispose();
+			new EditarDoadorFisico(a).open();
+		}
+		if(event.getSource().toString().equals("Button {Excluir Assistido}")) {
+			if(confirmacao()) {
+				fachada.excluirDoadorFisico(listaExibidaNaTabela.get(view.getTable().getSelectionIndex()).getId());
+				excluirLinhasDaTabela();
+				preencherTabelaDoadoresFisicos(obterTodosDoadoresFisicos());
+			}
+		}
+	}
+}
---
+++ src/main/java/view/GerenciarDoadoresFisicosView.java
@@ -0,0 +1,319 @@
+package view;
+
+import java.io.File;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.wb.swt.SWTResourceManager;
+
+import control.GerenciarDoadoresFisicosControle;
+
+public class GerenciarDoadoresFisicosView {
+	
+	GerenciarDoadoresFisicosControle controle;
+
+	protected Shell shlGerenciarDoadoresFisicos;
+	private Table table;
+	private TableColumn tblclmnId;
+	private TableColumn tblclmnNomeDoDoadorFisico;
+	private TableColumn tblclmnDataDeNascimento;
+	private TableColumn tblclmnCpf;
+	private Label lblSelecioneUmDoadorFisico;
+	private Label lblPesquisarDoadorFisico;
+	private Text tfPesquisa;
+	private Button btnCadastrar;
+	private Button btnEditar;
+	
+	private Button btnExcluir;
+	private Button btnCancelar;
+	
+	
+
+	
+	public GerenciarDoadoresFisicosView() {
+		this.controle = new GerenciarDoadoresFisicosControle(this);
+	}
+
+	public GerenciarDoadoresFisicosControle getControle() {
+		return controle;
+	}
+
+	public void setControle(GerenciarDoadoresFisicosControle controle) {
+		this.controle = controle;
+	}
+
+	public Shell getShlGerenciarDoadoresFisicos() {
+		return shlGerenciarDoadoresFisicos;
+	}
+
+	public void setShlGerenciarDoadoresFisicos(Shell shlGerenciarDoadoresFisicos) {
+		this.shlGerenciarDoadoresFisicos = shlGerenciarDoadoresFisicos;
+	}
+
+	public Table getTable() {
+		return table;
+	}
+
+	public void setTable(Table table) {
+		this.table = table;
+	}
+
+	public TableColumn getTblclmnId() {
+		return tblclmnId;
+	}
+
+	public void setTblclmnId(TableColumn tblclmnId) {
+		this.tblclmnId = tblclmnId;
+	}
+
+	public TableColumn getTblclmnNomeDoDoadorFisico() {
+		return tblclmnNomeDoDoadorFisico;
+	}
+
+	public void setTblclmnNomeDoDoadorFisico(TableColumn tblclmnNomeDoDoadorFisico) {
+		this.tblclmnNomeDoDoadorFisico = tblclmnNomeDoDoadorFisico;
+	}
+
+	public TableColumn getTblclmnDataDeNascimento() {
+		return tblclmnDataDeNascimento;
+	}
+
+	public void setTblclmnDataDeNascimento(TableColumn tblclmnDataDeNascimento) {
+		this.tblclmnDataDeNascimento = tblclmnDataDeNascimento;
+	}
+
+	public TableColumn getTblclmnCpf() {
+		return tblclmnCpf;
+	}
+
+	public void setTblclmnCpf(TableColumn tblclmnCpf) {
+		this.tblclmnCpf = tblclmnCpf;
+	}
+
+	public Label getLblSelecioneUmDoadorFisico() {
+		return lblSelecioneUmDoadorFisico;
+	}
+
+	public void setLblSelecioneUmDoadorFisico(Label lblSelecioneUmDoadorFisico) {
+		this.lblSelecioneUmDoadorFisico = lblSelecioneUmDoadorFisico;
+	}
+
+	public Label getLblPesquisarDoadorFisico() {
+		return lblPesquisarDoadorFisico;
+	}
+
+	public void setLblPesquisarDoadorFisico(Label lblPesquisarDoadorFisico) {
+		this.lblPesquisarDoadorFisico = lblPesquisarDoadorFisico;
+	}
+
+	public Text getTfPesquisa() {
+		return tfPesquisa;
+	}
+
+	public void setTfPesquisa(Text tfPesquisa) {
+		this.tfPesquisa = tfPesquisa;
+	}
+
+	public Button getBtnCadastrar() {
+		return btnCadastrar;
+	}
+
+	public void setBtnCadastrar(Button btnCadastrar) {
+		this.btnCadastrar = btnCadastrar;
+	}
+
+	public Button getBtnEditar() {
+		return btnEditar;
+	}
+
+	public void setBtnEditar(Button btnEditar) {
+		this.btnEditar = btnEditar;
+	}
+
+	public Button getBtnExcluir() {
+		return btnExcluir;
+	}
+
+	public void setBtnExcluir(Button btnExcluir) {
+		this.btnExcluir = btnExcluir;
+	}
+
+	public Button getBtnCancelar() {
+		return btnCancelar;
+	}
+
+	public void setBtnCancelar(Button btnCancelar) {
+		this.btnCancelar = btnCancelar;
+	}
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main() {
+		try {
+			GerenciarDoadoresFisicosView window = new GerenciarDoadoresFisicosView();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		Centralize.centralize(shlGerenciarDoadoresFisicos);
+		
+		Label lblGerenciarDoadroesFisicos = new Label(shlGerenciarDoadoresFisicos, SWT.NONE);
+		lblGerenciarDoadroesFisicos.setFont(SWTResourceManager.getFont("Segoe UI", 30, SWT.NORMAL));
+		lblGerenciarDoadroesFisicos.setBounds(66, 28, 475, 54);
+		lblGerenciarDoadroesFisicos.setText("Gerenciar Doadores Fisicos");
+		
+		lblSelecioneUmDoadorFisico = new Label(shlGerenciarDoadoresFisicos, SWT.NONE);
+		lblSelecioneUmDoadorFisico.setFont(SWTResourceManager.getFont("Segoe UI", 14, SWT.NORMAL));
+		lblSelecioneUmDoadorFisico.setBounds(196, 88, 334, 25);
+		lblSelecioneUmDoadorFisico.setText("Selecione ou pesquise um doador fisico");
+		
+		table = new Table(shlGerenciarDoadoresFisicos, SWT.BORDER | SWT.FULL_SELECTION);
+		table.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				btnEditar.setEnabled(true);
+				btnExcluir.setEnabled(true);
+			}
+		});
+		table.setLinesVisible(true);
+		table.setToolTipText("");
+		table.setFont(SWTResourceManager.getFont("Segoe UI", 14, SWT.NORMAL));
+		table.setBounds(10, 175, 694, 356);
+		table.setHeaderVisible(true);
+		
+		tblclmnId = new TableColumn(table, SWT.CENTER);
+		tblclmnId.setWidth(47);
+		tblclmnId.setText("ID");
+		tblclmnId.setResizable(false);
+		
+		tblclmnNomeDoDoadorFisico = new TableColumn(table, SWT.CENTER);
+		tblclmnNomeDoDoadorFisico.setWidth(275);
+		tblclmnNomeDoDoadorFisico.setText("Nome do Doador Fisico");
+		tblclmnNomeDoDoadorFisico.setResizable(false);
+		
+		tblclmnDataDeNascimento = new TableColumn(table, SWT.CENTER);
+		tblclmnDataDeNascimento.setWidth(187);
+		tblclmnDataDeNascimento.setText("Data de Nascimento");
+		tblclmnDataDeNascimento.setResizable(false);
+		
+		tblclmnCpf = new TableColumn(table, SWT.CENTER);
+		tblclmnCpf.setWidth(147);
+		tblclmnCpf.setText("CPF");
+		tblclmnCpf.setResizable(false);
+		
+		lblPesquisarDoadorFisico = new Label(shlGerenciarDoadoresFisicos, SWT.NONE);
+		lblPesquisarDoadorFisico.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
+		lblPesquisarDoadorFisico.setBounds(10, 136, 180, 30);
+		lblPesquisarDoadorFisico.setText("Pesquisar Assistido:");
+		
+		tfPesquisa = new Text(shlGerenciarDoadoresFisicos, SWT.BORDER);
+		tfPesquisa.setFont(SWTResourceManager.getFont("Segoe UI", 16, SWT.NORMAL));
+		tfPesquisa.setBounds(196, 133, 360, 36);
+		
+		Button btnPesquisar = new Button(shlGerenciarDoadoresFisicos, SWT.NONE);
+		btnPesquisar.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				controle.getEvent(arg0);
+			}
+		});
+		btnPesquisar.setFont(SWTResourceManager.getFont("Segoe UI", 14, SWT.NORMAL));
+		btnPesquisar.setImage(SWTResourceManager.getImage(new File("src/main/java/view/img/pesquisar.png").getAbsolutePath()));
+		btnPesquisar.setBounds(560, 133, 144, 36);
+		btnPesquisar.setText("Pesquisar");
+		
+		btnEditar = new Button(shlGerenciarDoadoresFisicos, SWT.NONE);
+		btnEditar.setEnabled(false);
+		btnEditar.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				controle.getEvent(arg0);
+			}
+		});
+		btnEditar.setImage(SWTResourceManager.getImage(new File("src/main/java/view/img/editar.png").getAbsolutePath()));
+		btnEditar.setFont(SWTResourceManager.getFont("Segoe UI", 14, SWT.NORMAL));
+		btnEditar.setBounds(132, 551, 208, 54);
+		btnEditar.setText("Editar Assistido");
+		
+		btnExcluir = new Button(shlGerenciarDoadoresFisicos, SWT.NONE);
+		btnExcluir.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				controle.getEvent(arg0);
+			}
+		});
+		btnExcluir.setEnabled(false);
+		btnExcluir.setFont(SWTResourceManager.getFont("Segoe UI", 14, SWT.NORMAL));
+		btnExcluir.setImage(SWTResourceManager.getImage(new File("src/main/java/view/img/lixeira.png").getAbsolutePath()));
+		btnExcluir.setBounds(346, 551, 208, 54);
+		btnExcluir.setText("Excluir Assistido");
+		
+		btnCadastrar = new Button(shlGerenciarDoadoresFisicos, SWT.NONE);
+		btnCadastrar.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				shlGerenciarDoadoresFisicos.dispose();
+				CadastroAssistidoView.main();
+			}
+		});
+		btnCadastrar.setFont(SWTResourceManager.getFont("Segoe UI", 14, SWT.NORMAL));
+		btnCadastrar.setImage(SWTResourceManager.getImage(new File("src/main/java/view/img/assistido-small.png").getAbsolutePath()));
+		btnCadastrar.setBounds(346, 615, 208, 54);
+		btnCadastrar.setText("Cadastrar Assistido");
+		
+		btnCancelar = new Button(shlGerenciarDoadoresFisicos, SWT.NONE);
+		btnCancelar.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				shlGerenciarDoadoresFisicos.dispose();
+			}
+		});
+		btnCancelar.setImage(SWTResourceManager.getImage(new File("src/main/java/view/img/cancelar.png").getAbsolutePath()));
+		btnCancelar.setFont(SWTResourceManager.getFont("Segoe UI", 14, SWT.NORMAL));
+		btnCancelar.setBounds(132, 615, 208, 54);
+		btnCancelar.setText("Cancelar");
+		
+		Label label = new Label(shlGerenciarDoadoresFisicos, SWT.NONE);
+		label.setImage(SWTResourceManager.getImage(new File("src/main/java/view/img/assistidos.png").getAbsolutePath()));
+		label.setBounds(560, 10, 137, 109);
+		
+		controle.preencherTabelaDoadoresFisicos(controle.obterTodosDoadoresFisicos());
+		shlGerenciarDoadoresFisicos.open();
+		shlGerenciarDoadoresFisicos.layout();
+		while (!shlGerenciarDoadoresFisicos.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shlGerenciarDoadoresFisicos = new Shell();
+		shlGerenciarDoadoresFisicos.setSize(730, 730);
+		shlGerenciarDoadoresFisicos.setText("Gerenciar Assistidos - ABRACE");
+		shlGerenciarDoadoresFisicos.setImage(SWTResourceManager.getImage(new File("src/main/java/view/img/ABRACE.png").getAbsolutePath()));
+
+	}
+
+}
