527d34f52b941ac963fbfec517c4f96f42d906ca
tagilaL <tagila.lima92@gmail.com>
2017-11-08 16:14:49 -0300

    Inclusão da logo da ABRACE nos relatórios, na classe RelatorioFacade.

--- src/main/java/facade/RelatorioFacade.java
+++ src/main/java/facade/RelatorioFacade.java
@@ -15,6 +15,7 @@ import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.Font.FontFamily;
+import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
@@ -38,6 +39,9 @@ public class RelatorioFacade {
 		document.open();
 		// ==================================================================================================//
 		
+		Image figura = Image.getInstance("src/main/java/view/img/ABRACE.png");
+		document.add(figura);
+		figura.setAlignment(Element.ALIGN_CENTER);
 		Cabecalho(document);
 
 		// ==================================================================================================//
