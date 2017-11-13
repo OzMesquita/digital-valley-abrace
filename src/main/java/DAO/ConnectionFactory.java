e3b222555e20ba0a07b58497b4de2c3df43b8a31
Gabriel <ggoncalvesmg@gmail.com>
2017-11-13 08:50:26 -0300

    Resolvendo problema do auto-incremente do derby

--- src/main/java/DAO/ConnectionFactory.java
+++ src/main/java/DAO/ConnectionFactory.java
@@ -4,16 +4,17 @@ import java.sql.*;
 
 public class ConnectionFactory {
 	private static String driver = "org.apache.derby.jdbc.EmbeddedDriver";
-	private String dbName = "localhost";// Lembre-se de mudar o nome do banco
-										// caso o seu seja diferente
+	private String dbName = "localhost";
 	private Connection connection;
 
 	public Connection getConnection() {
 		if (connection != null)
 			closeConnection();
 		try {
-			Class.forName(driver);
+			Class.forName(driver);//Se der problema na reinstalação mude shutdown=true para create=true, reinstale e mude para shutdown novamente 
 			connection = DriverManager.getConnection("jdbc:derby:" + dbName + ";create=true", "abrace", "abrace");
+			Statement s = connection.createStatement();
+			s.executeUpdate("CALL SYSCS_UTIL.SYSCS_SET_DATABASE_PROPERTY('derby.language.sequence.preallocator', '1')");
 			return connection;
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
