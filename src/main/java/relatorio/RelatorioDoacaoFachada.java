7e08f2636971a658d3a51df1e1cf4a5135fc83df
tagilaL <tagila.lima92@gmail.com>
2017-11-17 15:26:28 -0300

    Continuação do método de relatorioDoacaoMensal em RelatorioDoacaoFachada

--- src/main/java/relatorio/RelatorioDoacaoFachada.java
+++ src/main/java/relatorio/RelatorioDoacaoFachada.java
@@ -3,20 +3,25 @@ package relatorio;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
+import java.util.ArrayList;
 
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Font.FontFamily;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
 import model.Doacao;
+import model.PessoaFisica;
+import model.PessoaJuridica;
 
 public class RelatorioDoacaoFachada extends RelatorioFacade{
-	public void relatorioDoacaoMensal(List<Doacao> list) throws DocumentException, IOException{
+	public void relatorioDoacaoMensal(ArrayList<Doacao> list) throws DocumentException, IOException{
 
 		// ==================================================================================================//
 		// Criando o documento
@@ -36,10 +41,10 @@ public class RelatorioDoacaoFachada extends RelatorioFacade{
 
 		Cabecalho(document);
 
-		DateTimeFormatter formatadorDataCadastro = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+		DateTimeFormatter formatadorDataCadastro = DateTimeFormatter.ofPattern("MM");
 
 		Paragraph sub = new Paragraph(subtitulo,new Font(FontFamily.UNDEFINED,12,Font.BOLD));
-		Paragraph emissao = new Paragraph("Emissão: " + LocalDateTime.now().format(formatadorDataCadastro));
+		Paragraph emissao = new Paragraph("Referente ao mês " + LocalDateTime.now().format(formatadorDataCadastro));
 
 		sub.setSpacingBefore(20);
 
@@ -49,8 +54,90 @@ public class RelatorioDoacaoFachada extends RelatorioFacade{
 		document.add(sub);
 		document.add(emissao);
 		// ==================================================================================================//
+		
+		PdfPTable table2 = new PdfPTable(new float[] { 0.4f, 0.2f, 0.3f, 0.2f });
+		table2.setSpacingBefore(20);
 
+		PdfPCell doador      	= new PdfPCell(new Paragraph("Doador",new Font(FontFamily.UNDEFINED,12,Font.BOLD))),
+				 cpfOrCNPJ   	= new PdfPCell(new Paragraph("CPF/CNPJ",new Font(FontFamily.UNDEFINED,12,Font.BOLD))),
+				 dataDoacao     = new PdfPCell(new Paragraph("Data de doação",new Font(FontFamily.UNDEFINED,12,Font.BOLD))),
+				 valor          = new PdfPCell(new Paragraph("Valor doado",new Font(FontFamily.UNDEFINED,12,Font.BOLD)));
 
+		doador.setHorizontalAlignment(Element.ALIGN_CENTER);
+		cpfOrCNPJ.setHorizontalAlignment(Element.ALIGN_CENTER);
+		dataDoacao.setHorizontalAlignment(Element.ALIGN_CENTER);
+		valor.setHorizontalAlignment(Element.ALIGN_CENTER);
+
+		doador.setBackgroundColor(BaseColor.LIGHT_GRAY);
+		cpfOrCNPJ.setBackgroundColor(BaseColor.LIGHT_GRAY);
+		dataDoacao.setBackgroundColor(BaseColor.LIGHT_GRAY);
+		valor.setBackgroundColor(BaseColor.LIGHT_GRAY);
+
+		table2.addCell(doador);
+		table2.addCell(cpfOrCNPJ);
+		table2.addCell(dataDoacao);
+		table2.addCell(valor);
+
+		// --------------------------------------------------------------------------------------------------//
+		
+		DateTimeFormatter formatadorDataDoacao = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		float valorContado = 0;
+		
+		for (Doacao d : list) {
+			PdfPCell cellDoador 			  = new PdfPCell(new Paragraph()), 
+					 cellCPForCNPJ            = new PdfPCell(new Paragraph()), 
+					 cellDataDoacao           = new PdfPCell(new Paragraph()),
+					 cellValor			      = new PdfPCell(new Paragraph());
+
+			cellCPForCNPJ.setHorizontalAlignment(Element.ALIGN_CENTER);
+			cellDataDoacao.setHorizontalAlignment(Element.ALIGN_CENTER);
+			cellValor.setHorizontalAlignment(Element.ALIGN_CENTER);
+
+			if(d.getDoador() instanceof PessoaFisica){
+				cellDoador 				= new PdfPCell(new Paragraph(d.getDoador().getNome()));
+				cellCPForCNPJ          	= new PdfPCell(new Paragraph(((PessoaFisica)d.getDoador()).getCpf()));
+				cellDataDoacao          = new PdfPCell(new Paragraph(d.getData().format(formatadorDataDoacao)));
+				cellValor				= new PdfPCell(new Paragraph(((float)d.getValor())));
+
+				if (d.isAtivo()) {
+					table2.addCell(cellDoador);
+					table2.addCell(cellCPForCNPJ);
+					table2.addCell(cellDataDoacao);
+					table2.addCell(cellValor);
+					valorContado += d.getValor(); 
+				}
+			}else
+				if(d.getDoador() instanceof PessoaJuridica){
+					cellDoador 				= new PdfPCell(new Paragraph(((PessoaJuridica) d.getDoador()).getNomeFantasia()));
+					cellCPForCNPJ          	= new PdfPCell(new Paragraph(((PessoaJuridica) d.getDoador()).getCnpj()));
+					cellDataDoacao          = new PdfPCell(new Paragraph(d.getData().format(formatadorDataDoacao)));
+					cellValor				= new PdfPCell(new Paragraph(((float)d.getValor())));
+
+					if (d.isAtivo()) {
+						table2.addCell(cellDoador);
+						table2.addCell(cellCPForCNPJ);
+						table2.addCell(cellDataDoacao);
+						table2.addCell(cellValor);
+						valorContado += d.getValor(); 
+					}
+				}
+		}
+		PdfPCell cellValorTotal = new PdfPCell(new Paragraph("Valor total ",new Font(FontFamily.UNDEFINED,12,Font.BOLD)));
+		PdfPCell valorTotalDoado = new PdfPCell(new Paragraph(String.valueOf(valorContado)));
+		
+		cellValorTotal.setColspan(3);
+		cellValorTotal.setHorizontalAlignment(Element.ALIGN_RIGHT);
+		cellValorTotal.setBackgroundColor(BaseColor.LIGHT_GRAY);
+		
+		valorTotalDoado.setHorizontalAlignment(Element.ALIGN_CENTER);
+		valorTotalDoado.setBackgroundColor(BaseColor.LIGHT_GRAY);
+		
+		table2.addCell(cellValorTotal);
+		table2.addCell(valorTotalDoado);
+		table2.addCell("");
+		table2.addCell("");
+
+		document.add(table2);
 
 		document.close();
 	}
--- src/test/java/DAO/RelatorioDAOTest.java
+++ src/test/java/DAO/RelatorioDAOTest.java
@@ -2,6 +2,7 @@ package DAO;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 import com.itextpdf.text.DocumentException;
 
@@ -11,7 +12,10 @@ import exceptions.PessoaInvalidaException;
 import facade.DoadorFachada;
 import model.Doacao;
 import model.PessoaFisica;
+import model.PessoaJuridica;
 import relatorio.ReciboDoacaoFachada;
+import relatorio.RelatorioAssistidoFacade;
+import relatorio.RelatorioDoacaoFachada;
 import relatorio.RelatorioDoadoresFacade;
 
 public class RelatorioDAOTest {
@@ -28,13 +32,13 @@ public class RelatorioDAOTest {
 //			e.printStackTrace();
 //		}
 		
-		PessoaFisica isa = new PessoaFisica("Isabelly", "Jaguaruana", LocalDate.now(), "88996969696","88985858585", "isabelly@gmail.com", true, "06061487363", "20081547832", LocalDate.now());
-		Doacao doa = new Doacao(1, 200.00, LocalDate.now(), true, isa);
-		
-		ReciboDoacaoFachada recibo = new ReciboDoacaoFachada();
-		//recibo.reciboDoadorFisico(isa, doa);
-		
-		//TESTES RELATORIO DE DOADORES
+//		PessoaFisica isa = new PessoaFisica("Isabelly", "Jaguaruana", LocalDate.now(), "88996969696","88985858585", "isabelly@gmail.com", true, "06061487363", "20081547832", LocalDate.now());
+//		Doacao doa = new Doacao(1, 200.00, LocalDate.now(), true, isa);
+//		
+//		ReciboDoacaoFachada recibo = new ReciboDoacaoFachada();
+//		//recibo.reciboDoadorFisico(isa, doa);
+//		
+//		//TESTES RELATORIO DE DOADORES
 		DoadorFachada doadores = new DoadorFachada();
 		try {
 			new RelatorioDoadoresFacade().relatorioDeDoadores(doadores.getTodosDoadores(), true, true, true, true);
@@ -45,6 +49,17 @@ public class RelatorioDAOTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		//TESTES RELATORIO MENSAL
+//		try {
+//			new RelatorioDoacaoFachada().relatorioDoacaoMensal(new DoacaoDAO(new ConnectionFactory().getConnection()).listarDoacoes());
+//		} catch (DocumentException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		
 	}
 }
