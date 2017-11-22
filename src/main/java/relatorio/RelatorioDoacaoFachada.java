87bd098cffd4d45440ff92f8ccdbaf263f486e00
tagilaL <tagila.lima92@gmail.com>
2017-11-22 13:45:55 -0300

    Inclusão do método de geração de relatório anual em RelatorioDoacaoFachada.

--- src/main/java/relatorio/RelatorioDoacaoFachada.java
+++ src/main/java/relatorio/RelatorioDoacaoFachada.java
@@ -26,6 +26,7 @@ import model.PessoaJuridica;
 public class RelatorioDoacaoFachada extends RelatorioFacade{
 	public void relatorioDoacaoMensal(ArrayList<Doacao> list, LocalDate dataInicial, LocalDate dataFinal) throws DocumentException, IOException{
 
+
 		// ==================================================================================================//
 		// Criando o documento
 		// ==================================================================================================//
@@ -91,6 +92,129 @@ public class RelatorioDoacaoFachada extends RelatorioFacade{
 					d.getData().isAfter(dataInicial)||
 					d.getData().isEqual(dataInicial)||
 					d.getData().isEqual(dataFinal)) {
+				PdfPCell cellDoador 			  = new PdfPCell(new Paragraph()), 
+						 cellCPForCNPJ            = new PdfPCell(new Paragraph()), 
+						 cellDataDoacao           = new PdfPCell(new Paragraph()),
+						 cellValor			      = new PdfPCell(new Paragraph());
+	
+				if(d.getDoador() instanceof PessoaFisica){
+					cellDoador 				= new PdfPCell(new Paragraph(d.getDoador().getNome()));
+					cellCPForCNPJ          	= new PdfPCell(new Paragraph(((PessoaFisica)d.getDoador()).getCpf()));
+					cellDataDoacao          = new PdfPCell(new Paragraph(d.getData().format(formatadorDataDoacao)));
+					cellValor				= new PdfPCell(new Paragraph((NumberFormat.getCurrencyInstance().format(d.getValor()))));
+					
+					cellCPForCNPJ.setHorizontalAlignment(Element.ALIGN_CENTER);
+					cellDataDoacao.setHorizontalAlignment(Element.ALIGN_CENTER);
+	
+					if (d.isAtivo()) {
+						table2.addCell(cellDoador);
+						table2.addCell(cellCPForCNPJ);
+						table2.addCell(cellDataDoacao);
+						table2.addCell(cellValor);
+						valorContado += d.getValor(); 
+					}
+				}else
+					if(d.getDoador() instanceof PessoaJuridica){
+						cellDoador 				= new PdfPCell(new Paragraph(((PessoaJuridica) d.getDoador()).getNomeFantasia()));
+						cellCPForCNPJ          	= new PdfPCell(new Paragraph(((PessoaJuridica) d.getDoador()).getCnpj()));
+						cellDataDoacao          = new PdfPCell(new Paragraph(d.getData().format(formatadorDataDoacao)));
+						cellValor				= new PdfPCell(new Paragraph((NumberFormat.getCurrencyInstance().format(d.getValor()))));
+						
+						cellCPForCNPJ.setHorizontalAlignment(Element.ALIGN_CENTER);
+						cellDataDoacao.setHorizontalAlignment(Element.ALIGN_CENTER);
+	
+						if (d.isAtivo()) {
+							table2.addCell(cellDoador);
+							table2.addCell(cellCPForCNPJ);
+							table2.addCell(cellDataDoacao);
+							table2.addCell(cellValor);
+							valorContado += d.getValor(); 
+						}
+					}
+			}
+		}
+		PdfPCell cellValorTotal = new PdfPCell(new Paragraph("Valor total ",new Font(FontFamily.UNDEFINED,12,Font.BOLD)));
+		PdfPCell valorTotalDoado = new PdfPCell(new Paragraph(NumberFormat.getCurrencyInstance().format(valorContado)));
+		
+		cellValorTotal.setColspan(3);
+		cellValorTotal.setHorizontalAlignment(Element.ALIGN_RIGHT);
+		cellValorTotal.setBackgroundColor(BaseColor.LIGHT_GRAY);
+		
+		valorTotalDoado.setBackgroundColor(BaseColor.LIGHT_GRAY);
+		
+		table2.addCell(cellValorTotal);
+		table2.addCell(valorTotalDoado);
+		table2.addCell("");
+		table2.addCell("");
+
+		document.add(table2);
+
+		document.close();
+	}
+
+	public void relatorioDoacaoAnual(ArrayList<Doacao> list, int ano) throws DocumentException, IOException{
+
+
+		// ==================================================================================================//
+		// Criando o documento
+		// ==================================================================================================//
+		Document document = new Document();
+		String subtitulo = "Relatório anual de doações da ONG ABRACE Russas";
+		
+		PdfWriter.getInstance(document, gravarDocumento(subtitulo));
+
+		document.open();
+		// ==================================================================================================//
+
+
+		// ==================================================================================================//
+		// Cabecalho
+		// ==================================================================================================//
+
+		Cabecalho(document);
+
+		Paragraph sub = new Paragraph(subtitulo,new Font(FontFamily.UNDEFINED,12,Font.BOLD));
+		Paragraph emissao = new Paragraph("Referente ao ano de "+ ano);
+
+		sub.setSpacingBefore(20);
+
+		sub.setAlignment(Element.ALIGN_CENTER);
+		emissao.setAlignment(Element.ALIGN_CENTER);
+
+		document.add(sub);
+		document.add(emissao);
+		// ==================================================================================================//
+		
+		PdfPTable table2 = new PdfPTable(new float[] { 0.44f, 0.28f, 0.19f, 0.25f });
+		table2.setSpacingBefore(20);
+
+		PdfPCell doador      	= new PdfPCell(new Paragraph("Doador",new Font(FontFamily.UNDEFINED,12,Font.BOLD))),
+				 cpfOrCNPJ   	= new PdfPCell(new Paragraph("CPF/CNPJ",new Font(FontFamily.UNDEFINED,12,Font.BOLD))),
+				 dataDoacao     = new PdfPCell(new Paragraph("Data de doação",new Font(FontFamily.UNDEFINED,12,Font.BOLD))),
+				 valor          = new PdfPCell(new Paragraph("Valor doado",new Font(FontFamily.UNDEFINED,12,Font.BOLD)));
+
+		doador.setHorizontalAlignment(Element.ALIGN_CENTER);
+		cpfOrCNPJ.setHorizontalAlignment(Element.ALIGN_CENTER);
+		dataDoacao.setHorizontalAlignment(Element.ALIGN_CENTER);
+		valor.setHorizontalAlignment(Element.ALIGN_CENTER);
+
+		doador.setBackgroundColor(BaseColor.LIGHT_GRAY);
+		cpfOrCNPJ.setBackgroundColor(BaseColor.LIGHT_GRAY);
+		dataDoacao.setBackgroundColor(BaseColor.LIGHT_GRAY);
+		valor.setBackgroundColor(BaseColor.LIGHT_GRAY);
+
+		table2.addCell(doador);
+		table2.addCell(cpfOrCNPJ);
+		table2.addCell(dataDoacao);
+		table2.addCell(valor);
+
+		// --------------------------------------------------------------------------------------------------//
+		
+		DateTimeFormatter formatadorDataDoacao = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		float valorContado = 0;
+		
+		for (Doacao d : list) {
+			if(d.getData().getYear() == ano) {
 				PdfPCell cellDoador 			  = new PdfPCell(new Paragraph()), 
 						 cellCPForCNPJ            = new PdfPCell(new Paragraph()), 
 						 cellDataDoacao           = new PdfPCell(new Paragraph()),
