1a26806b93b1ac01731d22c2235c00cfe9dbe2cc
tagilaL <tagila.lima92@gmail.com>
2017-11-22 13:38:42 -0300

    Padronização da classe de teste de relatórios.

--- src/test/java/DAO/RelatorioDAOTest.java
+++ src/test/java/DAO/RelatorioDAOTest.java
@@ -22,37 +22,17 @@ public class RelatorioDAOTest {
 
 
 	public static void main(String[] args) throws DocumentException, IOException, PessoaInvalidaException, PessoaFisicaException, DoacaoInvalidaException {
-//		try {
-//			new RelatorioAssistidoFacade().relatorioDeAssistido(new AssistidoDAO(new ConnectionFactory().getConnection()).listaAssistido(),false,true);
-//		} catch (DocumentException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 		
-//		PessoaFisica isa = new PessoaFisica("Isabelly", "Jaguaruana", LocalDate.now(), "88996969696","88985858585", "isabelly@gmail.com", true, "06061487363", "20081547832", LocalDate.now());
-//		Doacao doa = new Doacao(1, 200.00, LocalDate.now(), true, isa);
-//		
-//		ReciboDoacaoFachada recibo = new ReciboDoacaoFachada();
-//		//recibo.reciboDoadorFisico(isa, doa);
-//		
-//		//TESTES RELATORIO DE DOADORES
-//		DoadorFachada doadores = new DoadorFachada();
-//		try {
-//			new RelatorioDoadoresFacade().relatorioDeDoadores(doadores.getTodosDoadores(), true, true, true, true);
-//		} catch (DocumentException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		
-		//TESTES RELATORIO MENSAL
+		//gerarRelatorioAssistido();
+		//gerarRelatorioDoadores();
+		//gerarRecibos();
+		//gerarRelatorioMensal();
+		gerarRelatorioAnual();
+	}
+
+	public static void gerarRelatorioAssistido(){
 		try {
-			new RelatorioDoacaoFachada().relatorioDoacaoMensal(new DoacaoDAO(new ConnectionFactory().getConnection()).listarDoacoes(),LocalDate.of(2017, 10, 20),LocalDate.of(2017, 10, 30));
+			new RelatorioAssistidoFacade().relatorioDeAssistido(new AssistidoDAO(new ConnectionFactory().getConnection()).listaAssistido(),false,true);
 		} catch (DocumentException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -60,6 +40,52 @@ public class RelatorioDAOTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+	}
+
+	public static void gerarRelatorioDoadores(){
+		//TESTES RELATORIO DE DOADORES
+		DoadorFachada doadores = new DoadorFachada();
+		try {
+			new RelatorioDoadoresFacade().relatorioDeDoadores(doadores.getTodosDoadores(), true, true, true, true);
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void gerarRecibos() throws PessoaInvalidaException, PessoaFisicaException, DocumentException, IOException, DoacaoInvalidaException{
+		PessoaFisica isa = new PessoaFisica("Isabelly", "Jaguaruana", LocalDate.now(), "88996969696","88985858585", "isabelly@gmail.com", true, "06061487363", "20081547832", LocalDate.now());
+		Doacao doa = new Doacao(1, 200.00, LocalDate.now(), true, isa);
+
+		ReciboDoacaoFachada recibo = new ReciboDoacaoFachada();
+		recibo.reciboDoadorFisico(isa, doa);
+
+	}
+	
+	public static void gerarRelatorioMensal() throws DoacaoInvalidaException {
+		try {
+			new RelatorioDoacaoFachada().relatorioDoacaoMensal(new DoacaoDAO(new ConnectionFactory().getConnection()).listarDoacoes(),LocalDate.of(2017, 11, 1),LocalDate.of(2017, 11, 30));
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void gerarRelatorioAnual() throws DoacaoInvalidaException {
+		try {
+			new RelatorioDoacaoFachada().relatorioDoacaoAnual(new DoacaoDAO(new ConnectionFactory().getConnection()).listarDoacoes(), LocalDate.now().getYear());
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
