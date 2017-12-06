5acccdb022e0d1be578237e4ed07e466eff6b2b3
Isabelly Lima <isabelly.lima132@gmail.com>
2017-12-06 14:48:16 -0300

    Criação da classe "EmitirRelatorioAssistidoeDoadores".

---
+++ src/main/java/exibirRelatorios/EmitirRelatorioAssistidoeDoadores.java
@@ -0,0 +1,49 @@
+package exibirRelatorios;
+
+import java.io.IOException;
+
+import com.itextpdf.text.DocumentException;
+
+import DAO.AssistidoDAO;
+import DAO.ConnectionFactory;
+import facade.DoadorFachada;
+import relatorio.RelatorioAssistidoFacade;
+import relatorio.RelatorioDoadoresFacade;
+
+public class EmitirRelatorioAssistidoeDoadores {
+	
+	public static void gerarRelatorioAssistido(){
+		try {
+			RelatorioAssistidoFacade r = new RelatorioAssistidoFacade();
+			r.relatorioDeAssistido(new AssistidoDAO(new ConnectionFactory().getConnection()).listaAssistido(),true,true);
+			r.abrirPDF();
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void gerarRelatorioDoadores(){
+		//TESTES RELATORIO DE DOADORES
+		DoadorFachada doadores = new DoadorFachada();
+		try {
+			RelatorioDoadoresFacade r = new RelatorioDoadoresFacade();
+			r.relatorioDeDoadores(doadores.getTodosDoadores(), true, true);
+			r.abrirPDF();
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		gerarRelatorioAssistido();
+		//gerarRelatorioDoadores();
+	}
+}
